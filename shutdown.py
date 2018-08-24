ee997943485c85c64c453ff5f7bfc4d695f015d7
Mark Constantine <MarkConstantConstantine@gmail.com>
2018-08-24 13:53:18 -0700

    Shutting down service now forks another process

--- shutdown.py
+++ shutdown.py
@@ -1,37 +1,56 @@
 import argparse
 import sys
 import os
+from multiprocessing import Process
 
-# TODO: config file?
+import time #temporary
 
 # we could define some base dir and use relative paths
 #BASE_DIR='/somewhere'
 
-# Finds all the running services (directories) in the script's root directory
+# Finds all the running services in the script's root directory.
 def find_all_running_services():
     # TODO: How to find if service is running?
-    
     services = []
 
     root = os.path.abspath(os.getcwd())
     for item in os.listdir(root):
         if os.path.isdir(os.path.join(root, item)):
-            #services.append(os.path.join(os.getcwd(), item)) # For absolute path
             services.append(item)
     
     return services
 
-def shutdown_service(services_to_shutdown_list):
-    for service in services_to_shutdown_list:
-        # TODO: Fork new process with dummy robin command. See multiprocessing module
-        # TODO: Try, except for robin command
-        print "Shutting down running service: {}".format(service)
+# Executes the necessary robin commands to shutdown a service.
+def shutdown_robin(service_directory):
+    # TODO: Execute and error handle the robin commands here
+    
+    print "Shutting down running service: {}".format(service_directory)
 
-def validate_services(input_services):
+    # This is for debugging purposes. Delete when replaced.
+    seconds = 10
+    print "Waiting {} seconds".format(seconds)
+    time.sleep(seconds) 
+
+# Forks multiple processes to execute the robin commands concurrently.
+def shutdown_service(services_to_shutdown_list):
+    processes = []
+    
+    for service in services_to_shutdown_list:
+        newProcess = Process(target=shutdown_robin, args={service})
+        newProcess.start()
+        processes.append(newProcess)
+
+    for process in processes:
+        process.join()
+
+    print "All provided services have been shut down."
+
+# Returns a list of services that can be shut down from a list of input
+def validate_services(input_services_list):
     running_services = find_all_running_services()
     valid = []        
     
-    for service in input_services:
+    for service in input_services_list:
         if service in running_services:
             valid.append(service)
         else:
@@ -39,32 +58,35 @@ def validate_services(input_services):
     
     return valid
 
-# Defining/parsing command-line arguments
-parser = argparse.ArgumentParser(description="Shutdown Deployment Script")
-parser.add_argument("-all", required=False, action="store_true", help="shutdown all running services")
-parser.add_argument("-file", type=str, nargs="?", help="read service names from the file")
-parser.add_argument("-services", type=str, nargs="+", help="list of services available to shutdown")
-args = parser.parse_args()
+if __name__ == "__main__":
+    # Defining/parsing command-line arguments
+    parser = argparse.ArgumentParser(description="Shutdown Deployment Script")
+    parser.add_argument("-all", required=False, action="store_true", help="shutdown all running services")
+    parser.add_argument("-file", type=str, nargs="?", help="read service names from the file")
+    parser.add_argument("-services", type=str, nargs="+", help="list of services available to shutdown")
+    args = parser.parse_args()
 
-# Priority order:
-#   1. -all
-#   2. -file
-#   3. -services
-if ((args.all) and (args.file == None) and (args.services == None)):        # -all
-    shutdown_service(find_all_running_services())
-elif ((not args.all) and (args.file != None) and (args.services == None)):  # -file
-    try:
-        services_list = []
-        with open(args.file) as fin:
-            for service in fin:
-                services_list.append(service.replace("\n", ""))
-        shutdown_service(validate_services(services_list))
-    except IOError:
-        print "Failed to open file: {}".format(args.file)
-        print "Stopping..."
-        sys.exit(1)
-elif ((not args.all) and (args.file == None) and (args.services != None)):  # -services
-    valid_running_services = validate_services(args.services)
-    shutdown_service(valid_running_services)
-else:
-    parser.print_help()
+    # Priority order:
+    #   1. -all
+    #   2. -file
+    #   3. -services
+    if ((args.all) and (args.file == None) and (args.services == None)):        # -all
+        shutdown_service(find_all_running_services())
+    elif ((not args.all) and (args.file != None) and (args.services == None)):  # -file
+        try:
+            services_list = []
+            with open(args.file) as fin:
+                for service in fin:
+                    services_list.append(service.replace("\n", ""))
+            shutdown_service(validate_services(services_list))
+        except IOError:
+            print "Failed to open file: {}".format(args.file)
+            print "Stopping..."
+            sys.exit(1)
+    elif ((not args.all) and (args.file == None) and (args.services != None)):  # -services
+        valid_running_services = validate_services(args.services)
+        shutdown_service(valid_running_services)
+    else:
+        parser.print_help()
+
+print "Exiting"
