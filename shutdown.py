74e513327c4ddfe34a86f5eafe0b5cab3f7f7e51
Mark Constantine <MarkConstantConstantine@gmail.com>
2018-08-24 10:55:56 -0700

    Rough outline for shutdown complete

--- shutdown.py
+++ shutdown.py
@@ -7,8 +7,10 @@ import os
 # we could define some base dir and use relative paths
 #BASE_DIR='/somewhere'
 
-# Finds all the services (directories) in the script's root directory
-def find_all_services():
+# Finds all the running services (directories) in the script's root directory
+def find_all_running_services():
+    # TODO: How to find if service is running?
+    
     services = []
 
     root = os.path.abspath(os.getcwd())
@@ -19,39 +21,50 @@ def find_all_services():
     
     return services
 
-def shutdown_service(service_list):
-    # TODO: loop through service_list and loop through find_all_services()
-        # if serviceName is in both service_list and find_all_services()
-            # fork new process
-            # fork process to shutdown
-    return
+def shutdown_service(services_to_shutdown_list):
+    for service in services_to_shutdown_list:
+        # TODO: Fork new process with dummy robin command. See multiprocessing module
+        # TODO: Try, except for robin command
+        print "Shutting down running service: {}".format(service)
 
-def shutdown_service_from_file(fileName):
-    with open(fileName) as fin:
-        for line in fin:
-            #shutdown_service(line)
-            pass
+def validate_services(input_services):
+    running_services = find_all_running_services()
+    valid = []        
+    
+    for service in input_services:
+        if service in running_services:
+            valid.append(service)
+        else:
+            print "\'{}\' is not a running service".format(service)
+    
+    return valid
 
-def shutdown_all():
-    pass
-
+# Defining/parsing command-line arguments
 parser = argparse.ArgumentParser(description="Shutdown Deployment Script")
 parser.add_argument("-all", required=False, action="store_true", help="shutdown all running services")
 parser.add_argument("-file", type=str, nargs="?", help="read service names from the file")
 parser.add_argument("-services", type=str, nargs="+", help="list of services available to shutdown")
-
 args = parser.parse_args()
-        
-if (args.all):
-    print "Shutting down all running services..."
-    shutdown_all()
-elif (args.file != None):
-    filename = str(args.file)
-    print "Shutting down running services specified in file: " + filename    
-    shutdown_service_from_file(filename)
-elif (args.services != None):
-    print "List of Services: " + str(args.services)
-    shutdown_service(args.services)
+
+# Priority order:
+#   1. -all
+#   2. -file
+#   3. -services
+if ((args.all) and (args.file == None) and (args.services == None)):        # -all
+    shutdown_service(find_all_running_services())
+elif ((not args.all) and (args.file != None) and (args.services == None)):  # -file
+    try:
+        services_list = []
+        with open(args.file) as fin:
+            for service in fin:
+                services_list.append(service.replace("\n", ""))
+        shutdown_service(validate_services(services_list))
+    except IOError:
+        print "Failed to open file: {}".format(args.file)
+        print "Stopping..."
+        sys.exit(1)
+elif ((not args.all) and (args.file == None) and (args.services != None)):  # -services
+    valid_running_services = validate_services(args.services)
+    shutdown_service(valid_running_services)
 else:
     parser.print_help()
-    pass
